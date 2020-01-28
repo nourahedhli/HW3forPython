cead25f2e4fbab012cc063a91264fd0cfe12021c
Noura.Hedhli <nh98@njit.edu>
2020-01-28 15:24:21 -0500

    Task: addition

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -1,17 +1,18 @@
 import unittest
 
-
+from Calculator.Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
 
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
 
-
-
-
-
-    def test_results_property(self):
-        self.assertEqual(self.calculator.result, 0)
+    def test_calculator_addition(self):
+        calculator = Calculator()
+        result = calculator.addition(1, 2)
+        self.assertEqual(3, result)
 
 
 if __name__ == '__main__':
