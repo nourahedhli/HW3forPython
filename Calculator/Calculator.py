e06dfa810d659805aba45cd997755664a6222ec9
Noura.Hedhli <nh98@njit.edu>
2020-02-09 13:14:21 -0500

    Tast; Travis CI

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -2,20 +2,42 @@ import math
 
 from MathOperations.additon import Addition
 from MathOperations.subtraction import Subtraction
+from MathOperations.multiplication import Multiplication
+from MathOperations.division import Division
+from MathOperations.exponentiation import Exponent
+from MathOperations.nthroot import nthRoot
+from MathOperations.log import Log
 
 class Calculator:
     Result = 0
+
     def __init__(self):
         pass
 
     def Sum(self, a, b):
-        self.Result= Addition.sum(a,b)
+        self.Result = Addition.sum(a, b)
         return self.Result
-
 
     def Difference(self, a, b):
-        self.Result = Subtraction.difference(a,b)
+        self.Result = Subtraction.difference(a, b)
         return self.Result
 
+    def Product(self, a, b):
+        self.Result = Multiplication.product(a, b, )
+        return self.Result
 
+    def Fraction(self, a, b):
+        self.Result = Division.fraction(a, b)
+        return self.Result
+
+    def Power(self, a, b):
+        self.Result = Exponent.power(a, b)
+        return self.Result
+    def Root (self,a,b):
+        self.Result=nthRoot.NthRoot(a,b)
+        return self.Result
+
+    def Log (self,a,b):
+        self.Result= Log.logarithm(a,b)
+        return self.Result
 
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -5,31 +5,20 @@ from Calculator.Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
-    def setUp(self) :
+    def setUp(self):
         self.calculator = Calculator()
 
-
     def test_instantiate_calculator(self):
-
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_calculator_return_sum(self):
-
-        result = self.calculator.Sum(1, 2)
-        self.assertEqual(3, result)
-
-    def test_calculator_return_difference(self):
-
-        result = self.calculator.Difference(1, 2)
-        self.assertEqual(-1, result)
+        self.assertEqual(3, self.calculator.Sum(1, 2))
 
     def test_calculator_result_access_difference_result(self):
-
         self.calculator.Difference(1, 2)
         self.assertEqual(-1, self.calculator.Result)
 
     def test_calculator_result_access_Sum_result(self):
-
         self.calculator.Sum(1, 2)
         self.assertEqual(3, self.calculator.Result)
 
@@ -38,9 +27,30 @@ class MyTestCase(unittest.TestCase):
         calculator2 = Calculator()
 
         calculator1.Sum(1, 2)
-        calculator2.Difference(3,4)
-        self.calculator.Sum(calculator1.Sum(1,2), calculator2.Difference(3,4))
+        calculator2.Difference(3, 4)
+        self.calculator.Sum(calculator1.Sum(1, 2), calculator2.Difference(3, 4))
         self.assertEqual(2, self.calculator.Result)
+
+    def test_product_result_calculator(self):
+        self.calculator.Product(2, 4)
+        self.assertEqual(8, self.calculator.Result)
+
+    def test_division_result_calculator(self):
+        self.calculator.Fraction(6, 3)
+        self.assertEqual(2, self.calculator.Result)
+
+    def test_power_result_calculator(self):
+        self.calculator.Power(2, 3)
+        self.assertEqual(8, self.calculator.Result)
+
+    def test_root_result_calculator(self):
+        self.calculator.Root(2, 16)
+        self.assertEqual(4, self.calculator.Result)
+
+    def test_log_result_calculator(self):
+        float(self.calculator.Log(2, 4))
+        self.assertEqual(2, self.calculator.Result)
+
 
 if __name__ == '__main__':
     unittest.main()
